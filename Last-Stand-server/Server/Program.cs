62fde8bc81e8ecb73352b7136a248b2791f59409
Sean-mn <b01080080329@gmail.com>
2025-06-13 00:54:09 +0900

    fix: Program.cs 수정
    Jwt 설정 및 Cors 설정
    
    #1

--- Last-Stand-server/Server/Program.cs
+++ Last-Stand-server/Server/Program.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Server.Configuration;
 using Server.Repository;
 using Server.Service;
 using Server.Repository.Interface;
@@ -5,14 +9,52 @@ using Server.Service.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<JwtSetting>(
+    builder.Configuration.GetSection("JwtSettings"));
+
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 
+var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSetting>();
+var key = Encoding.UTF8.GetBytes(jwtSettings.SecretKey);
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = jwtSettings.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowUnity", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseCors("AllowUnity");
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
