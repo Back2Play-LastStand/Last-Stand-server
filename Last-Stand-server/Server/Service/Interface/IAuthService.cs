22debba687b276f351fa28f82767f7ccfc0b821e
Sean-mn <b01080080329@gmail.com>
2025-06-13 00:22:30 +0900

    feat: AuthService 구현
    #1

---
+++ Last-Stand-server/Server/Service/Interface/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using Server.Model.Account.Entity;
+
+namespace Server.Service.Interface;
+
+public interface IAuthService
+{
+    Task<bool> RegisterAsync(string playerId, string password);
+    Task<string?> LoginAsync(string playerId, string password);
+}
