feb6f402afbfda9f4081f7c367cda25047ecaf5a
Sean-mn <b01080080329@gmail.com>
2025-06-13 08:01:21 +0900

    feat: 요청/응답 Dto 생성
    아이디 찾기 및 비밀번호 변경 Dto 생성
    
    #4

---
+++ Last-Stand-server/Server/Model/Account/Dto/Request/ResetPasswordRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Model.Account.Dto.Request;
+
+public class ResetPasswordRequest
+{
+    public string PlayerId  { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string NewPassword { get; set; } = null!;
+}
