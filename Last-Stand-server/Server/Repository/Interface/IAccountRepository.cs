d813d00c02167fd426ad3ae8725497e0d19bb8e8
Sean-mn <b01080080329@gmail.com>
2025-06-13 00:15:42 +0900

    feat: AccountRepository 생성
    IAccountRepository 인터페이스 및 AccountRepository 클래스 구현
    
    #1

---
+++ Last-Stand-server/Server/Repository/Interface/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using Server.Model.Account.Entity;
+
+namespace Server.Repository.Interface;
+
+public interface IAccountRepository
+{
+    Task<bool> CheckExistsAsync(string playerId);
+    Task AddAccountAsynce(PlayerLoginData account);
+    Task<PlayerLoginData?> FindByPlayerIdAsync(string playerId);
+}
