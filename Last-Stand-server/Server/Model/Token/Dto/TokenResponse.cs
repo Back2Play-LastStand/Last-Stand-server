c6bd9bdbe91f8b71725ff0599d5e65ef3471fda2
Sean-mn <b01080080329@gmail.com>
2025-06-13 01:05:12 +0900

    feat: TokenResponse Dto 생성
    #1

---
+++ Last-Stand-server/Server/Model/Token/Dto/TokenResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Model.Token.Dto;
+
+public class TokenResponse
+{
+    public string AccessToken { get; set; } = null!;
+    public string RefreshToken { get; set; } = null!;
+    public DateTime AccessTokenExpiration { get; set; }
+    public DateTime RefreshTokenExpiration { get; set; }
+}
