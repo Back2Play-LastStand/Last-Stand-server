bd11736abd4441c15f3af3ed22bc94e17b705e78
Sean-mn <b01080080329@gmail.com>
2025-06-13 00:38:37 +0900

    modift: 인증 로직 DI 주입
    #1

--- Last-Stand-server/Server/Program.cs
+++ Last-Stand-server/Server/Program.cs
@@ -1,4 +1,12 @@
+using Server.Repository;
+using Server.Service;
+using Server.Repository.Interface;
+using Server.Service.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 
 builder.Services.AddControllers();
 
