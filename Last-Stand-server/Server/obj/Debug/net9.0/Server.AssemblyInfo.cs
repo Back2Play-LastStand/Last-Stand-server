08f4f8969e097b783d4f0bb87d26544f024ab4f0
Sean-mn <b01080080329@gmail.com>
2025-06-13 01:06:50 +0900

    feat: Redis 관리 패키지 설치
    #1

--- Last-Stand-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Last-Stand-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd11736abd4441c15f3af3ed22bc94e17b705e78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6bd9bdbe91f8b71725ff0599d5e65ef3471fda2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
