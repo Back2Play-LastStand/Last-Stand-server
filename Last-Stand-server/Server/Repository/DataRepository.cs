e9ebd721d9c2ed4e2ab8179344253a8670f881b8
Sean-mn <b01080080329@gmail.com>
2025-06-13 11:58:30 +0900

    feat: DataRepositoy 생성
    #11

---
+++ Last-Stand-server/Server/Repository/DataRepository.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using MySqlConnector;
+using Server.Model.Data.Player.Entity;
+using Server.Repository.Interface;
+
+namespace Server.Repository;
+
+public class DataRepository : IDataRepository
+{
+    private readonly string _connectionString;
+
+    public DataRepository(IConfiguration config)
+    {
+        _connectionString = config.GetConnectionString("DefaultConnection");
+    }
+
+    private MySqlConnection CreateConnection() => new(_connectionString);
+    
+    public async Task<PlayerData?> GetByPlayerIdAsync(string playerId)
+    {
+        const string sql = @"
+            SELECT id, player_id AS PlayerId, player_name AS PlayerName, is_new_account AS IsNewAccount
+            FROM last_stand_player_data
+            WHERE player_id = @PlayerId;
+        ";
+        using var connection = CreateConnection();
+        await connection.OpenAsync();
+        
+        return await connection.QueryFirstOrDefaultAsync<PlayerData>(sql,  new { PlayerId = playerId });
+    }
+
+    public async Task<bool> IsNameTakenAsync(string playerName)
+    {
+        const string sql = @"
+            SELECT COUNT(1)
+            FROM last_stand_player_data
+            WHERE player_name = @PlayerName;
+        ";
+
+        using var connection = CreateConnection();
+        await connection.OpenAsync();
+
+        var count = await connection.ExecuteScalarAsync<int>(sql, new { PlayerName = playerName });
+        return count > 0;
+    }
+
+    public async Task AddPlayerDataAsync(PlayerData data)
+    {
+        const string sql = @"
+        INSERT INTO last_stand_player_data (player_id, player_name, is_new_account)
+        VALUES (@PlayerId, @PlayerName, @IsNewAccount);
+    ";
+        
+        using var connection = CreateConnection();
+        await connection.OpenAsync();
+
+        await connection.ExecuteAsync(sql, data);
+    }
+}
