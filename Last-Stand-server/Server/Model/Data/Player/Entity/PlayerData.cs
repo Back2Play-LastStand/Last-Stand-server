ebe38c15edf026bb2dc43b3938b89c5331fe2811
Sean-mn <b01080080329@gmail.com>
2025-06-13 11:49:56 +0900

    feat: PlayerData 엔티티 생성
    #11

---
+++ Last-Stand-server/Server/Model/Data/Player/Entity/PlayerData.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Model.Data.Player.Entity;
+
+[Table("last_stand_player_data")]
+public class PlayerData
+{
+    [Key]
+    [Required]
+    [Column("id")]
+    public int Id { get; set; }
+    
+    [Required]
+    [Column("player_id")]
+    [MaxLength(15)]
+    public string PlayerId { get; set; } = null!;
+    
+    [Required]
+    [Column("player_name")]
+    [MaxLength(50)]
+    public string PlayerName { get; set; } = null!;
+
+    [Required] [Column("is_new_account")]
+    public bool IsNewAccount { get; set; }
+}
