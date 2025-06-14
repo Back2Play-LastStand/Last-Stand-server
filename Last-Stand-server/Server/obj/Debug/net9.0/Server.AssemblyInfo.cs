41781a4aa307924acdf53e5444d078bd07dfe6b3
Seanyee1227 <b01080080329@gmail.com>
2025-06-15 00:59:06 +0900

    .

--- Last-Stand-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Last-Stand-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5177c4a9e5669b97035f03cd73546b06fa2eca0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c36375b71bc973b60851d42171703b91c43908")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
