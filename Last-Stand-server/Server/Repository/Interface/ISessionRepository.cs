43bddc2ad4b0dc54da0c13578f6818debf9743e2
Sean-mn <b01080080329@gmail.com>
2025-06-16 01:38:17 +0900

    feat: SessionRepository 생성
    #18

---
+++ Last-Stand-server/Server/Repository/Interface/ISessionRepository.cs
@@ -0,0 +1,10 @@
+﻿using Server.Model.Account.Entity;
+
+namespace Server.Repository.Interface;
+
+public interface ISessionRepository
+{
+    Task CreateSessionAsync(AccountSession session);
+    Task<AccountSession?> GetValidSessionAsync(string sessionId);
+    Task DeleteSessionAsync(string sessionId);
+}
---
+++ Last-Stand-server/Server/Repository/SessionRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using MySqlConnector;
+using Server.Model.Account.Entity;
+using Server.Repository.Interface;
+
+namespace Server.Repository;
+
+public class SessionRepository : ISessionRepository
+{
+    private readonly string _connectionString;
+
+    public SessionRepository(IConfiguration config)
+    {
+        _connectionString = config.GetConnectionString("AccountDbConnection");
+    }
+    
+    private MySqlConnection CreateConnection() => new(_connectionString);
+    
+    public async Task CreateSessionAsync(AccountSession session)
+    {
+        const string sql = @"
+            INSERT INTO account_session (session_id, account_id, created_at, expires_at)
+            VALUES (@SessionId, @AccountId, @CreatedAt, @ExpiresAt);
+            ";
+        
+        await using var connection  = CreateConnection();
+        
+        await connection.ExecuteAsync(sql, session);
+    }
+
+    public async Task<AccountSession?> GetValidSessionAsync(string sessionId)
+    {
+        const string sql = @"
+            SELECT * FROM account_session
+            WHERE session_id = @SessionId AND expires_at > UTC_TIMESTAMP()";
+        
+        await using var connection  = CreateConnection();
+        return await connection.QueryFirstOrDefaultAsync<AccountSession>(sql, new { SessionId = sessionId });
+    }
+
+    public async Task DeleteSessionAsync(string sessionId)
+    {
+        const string sql = @"
+            DELETE FROM account_session
+            WHERE session_id = @SessionId";
+        
+        await using var connection = CreateConnection();
+        await connection.ExecuteAsync(sql, new { SessionId = sessionId });
+    }
+}
