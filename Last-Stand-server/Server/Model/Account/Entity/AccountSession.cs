560546cf829c4535f0a4c4a7d58ae780aea19988
Sean-mn <b01080080329@gmail.com>
2025-06-16 01:27:42 +0900

    feat: AccountSession 엔티티 생성
    #18

---
+++ Last-Stand-server/Server/Model/Account/Entity/AccountSession.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Model.Account.Entity;
+
+[Table("account_session")]
+public class AccountSession
+{
+    [Key]
+    [Column("session_id")]
+    [MaxLength(64)]
+    public string SessionId { get; set; } = Guid.NewGuid().ToString();
+    
+    [Required]
+    [Column("account_id")]
+    public int AccountId { get; set; }
+    
+    [Required]
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+    
+    [Required]
+    [Column("expires_at")]
+    public DateTime ExpiresAt { get; set; }
+}
