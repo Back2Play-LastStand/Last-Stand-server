5dc3e1c232c171639c0b2130cb381dbf93d410d6
Sean-mn <b01080080329@gmail.com>
2025-06-16 12:26:06 +0900

    feat: 인증 관련 서비스 구현
    #24

---
+++ Last-Stand-server/Server/Service/Interface/IVerificationService.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Service.Interface;
+
+public interface IVerificationService
+{
+    Task StoreVerificationCodeAsync(string email, string code);
+    Task<bool> VerifyCodeAsync(string email, string code);
+    Task RemoveCodeAsync(string email);
+}
---
+++ Last-Stand-server/Server/Service/VerificationService.cs
@@ -0,0 +1,34 @@
+﻿using Server.Service.Interface;
+using StackExchange.Redis;
+
+namespace Server.Service;
+
+public class VerificationService : IVerificationService
+{
+    private readonly IDatabase _redis;
+    private readonly TimeSpan _expiration =  TimeSpan.FromHours(3);
+
+    public VerificationService(IConnectionMultiplexer  redis)
+    {
+        _redis = redis.GetDatabase();
+    }
+    
+    public async Task StoreVerificationCodeAsync(string email, string code)
+    {
+        await _redis.StringSetAsync(email, code,  _expiration);
+    }
+
+    public async Task<bool> VerifyCodeAsync(string email, string code)
+    {
+        var storedCode = await _redis.StringGetAsync(email);
+        if (storedCode.IsNullOrEmpty)
+            return false;
+        
+        return storedCode == code;
+    }
+
+    public async Task RemoveCodeAsync(string email)
+    {
+        await _redis.KeyDeleteAsync(email);
+    }
+}
