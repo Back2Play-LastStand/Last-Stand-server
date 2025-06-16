7c5132b91e24be67492b37310793f7131a5cc925
Sean-mn <b01080080329@gmail.com>
2025-06-16 22:28:51 +0900

    feat: 회원가입 로직 예외 처리

---
+++ Last-Stand-server/Server/DefinedException/DuplicateEmailException.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.DefinedException;
+
+public class DuplicateEmailException : Exception
+{
+    public DuplicateEmailException() : base("Email already exists.")
+    {
+    }
+
+    public DuplicateEmailException(string message) : base(message)
+    {
+    }
+}
---
+++ Last-Stand-server/Server/DefinedException/DuplicatePlayerIdException.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.DefinedException;
+
+public class DuplicatePlayerIdException : Exception
+{
+    public DuplicatePlayerIdException() : base("PlayerId already exists.")
+    {
+    }
+
+    public DuplicatePlayerIdException(string message) : base(message)
+    {
+    }
+}
